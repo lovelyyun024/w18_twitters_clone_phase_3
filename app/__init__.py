5e2c8c5002850064804d6e3191f2b64d0bb5266c
Yun Zhang <zhangyun024@gmail.com>
2023-12-05 14:51:21 -0800

    phase3

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from .config import Config
 from .tweets import tweets
 import random
 # !!END
+from .form.form import TweetForm
+
 
 app = Flask(__name__)
 
@@ -26,3 +28,11 @@ def feed():
     """
     return render_template('feed.html', tweets=tweets)
 # !!END
+
+
+@app.route("/new", method = ["GET", "POST"])
+def new():
+    form = TweetForm()
+    return render_template('new_tweet.html', form=form)
+
+    
